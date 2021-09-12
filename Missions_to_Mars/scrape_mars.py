aea78d05ae84dd7b5753c4133152d8ccc73f054b
Amber <amberleeBME@outlook.com>
2021-09-11 21:22:46 -0400

    convert jupyter notebook to scrape_mars.py

---
+++ Missions_to_Mars/scrape_mars.py
@@ -0,0 +1,47 @@
+# Import Dependencies
+import pandas as pd
+import requests
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+from webdriver_manager.chrome import ChromeDriverManager
+
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+url = 'http://redplanetscience.com'
+browser.visit(url)
+soup = bs(browser.html, 'lxml')
+
+latestTitle=soup.find_all('div',class_="content_title")[0].text
+latestTeaser=soup.find_all('div',class_="article_teaser_body")[0].text
+
+url = 'https://spaceimages-mars.com'
+browser.visit(url)
+soup = bs(browser.html, 'lxml')
+
+partial_url = soup.find('a',class_='showimg fancybox-thumbs')['href']
+featured_image_url=url +'/'+partial_url
+browser.quit()
+
+url = 'https://galaxyfacts-mars.com'
+table = pd.read_html(url)[1]
+table.rename(columns={0:'Fact',1:'Value'},inplace=True)
+
+html_table= table.to_html()
+html_table=html_table.replace('\n', '')
+html_table
+
+url = 'https://marshemispheres.com/'
+response = requests.get(url)
+soup = bs(response.text, 'lxml')
+results = soup.find_all('div', class_='description')
+
+hemisphere_image_urls=[]
+for each in results:
+    title = each.h3.text    
+    response = requests.get(f'https://marshemispheres.com/{each.a["href"]}')
+    soup = bs(response.text,'lxml')
+    hemiResult=soup.find_all('dd')[1].a['href']
+    hemisphere_image_urls.append({
+        "title":title[0:-9],
+        "img_url":f'https://marshemispheres.com/{hemiResult}'
+        })
